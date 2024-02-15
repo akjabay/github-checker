084f1cec8a2fc064b2fa0c8e37524f6653fb2084
x <x@g.com>
2024-02-15 23:38:09 +0300

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+const axios = require('axios').default;
+
+const initBot = async function ({ beginFrom = 538 } = []) {
+
+
+    try {
+
+        const data = require('./github-1.js');
+
+        for await (const [index, user] of data.reverse().entries()) {
+
+            if (index > beginFrom) {
+
+                try {
+
+                    const res = await axios.get('https://api.github.com/users/' + user.identity);
+
+                    if (!res.data.id) {
+                        console.log(user.identity, '!!!!new!!!!')
+                    } else {
+                        console.log(index, user.identity)
+                    }
+                } catch (error) {
+                    console.log('error')
+                    initBot({ beginFrom: index })
+                    return;
+                }
+            }
+        }
+
+    } catch (error) {
+        console.log('error')
+    }
+
+
+}
+
+
+initBot()
